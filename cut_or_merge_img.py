2745f2d983be47eaea27ecf0b331bbcb4c2073e1
tongchangD <36293393+tongchangD@users.noreply.github.com>
2021-07-29 09:04:11 +0800

    Update cut_or_merge_img.py

--- cut_or_merge_img.py
+++ cut_or_merge_img.py
@@ -1,25 +1,48 @@
-def cut_big_pic(img_path, source_min_img_path):
-    img = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), -1)
-    height, width, depth = img.shape
-    height_cut = height // 256
-    width_cut = width // 256
-    pattern_height = 256
-    pattern_width = 256
-    for j in range(height_cut):
-        for i in range(width_cut):
-            #  切割的时候如果是靠边的最后一个就将图片加宽，保持原本的尺寸
-            left_h = j * pattern_height
-            left_w = i * pattern_width
-            right_h = left_h + pattern_height
-            right_w = left_w + pattern_width
-            if i == width_cut - 1 or j == height_cut - 1:
-                if i == width_cut - 1:
-                    right_w = width if width >= width_cut * 256 else (left_w + pattern_width)
-                if j == height_cut - 1:
-                    right_h = height if height >= height_cut * 256 else (left_h + pattern_height)
-            sub_img = img[left_h: right_h, left_w: right_w]
-            cv2.imencode('.jpg', sub_img)[1].tofile(
-                os.path.join(source_min_img_path, os.path.split(img_path)[1][:-4] + '_{}{}'.format(j, i) + '.jpg'))
+def cut_big_pic(img_path, source_min_img_path, height_cut, width_cut):
+    if height_cut == 0 or width_cut == 0:
+        img = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), -1)
+        height, width, depth = img.shape
+        height_cut = height // 256
+        width_cut = width // 256
+        pattern_height = 256
+        pattern_width = 256
+        for j in range(height_cut):
+            for i in range(width_cut):
+                #  切割的时候如果是靠边的最后一个就将图片加宽，保持原本的尺寸
+                left_h = j * pattern_height
+                left_w = i * pattern_width
+                right_h = left_h + pattern_height
+                right_w = left_w + pattern_width
+                if i == width_cut - 1 or j == height_cut - 1:
+                    if i == width_cut - 1:
+                        right_w = width if width >= width_cut * 256 else (left_w + pattern_width)
+                    if j == height_cut - 1:
+                        right_h = height if height >= height_cut * 256 else (left_h + pattern_height)
+                sub_img = img[left_h: right_h, left_w: right_w]
+                cv2.imencode('.jpg', sub_img)[1].tofile(
+                    os.path.join(source_min_img_path, os.path.split(img_path)[1][:-4] + '_{}{}'.format(j, i) + '.jpg'))
+    else:
+        img = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), -1)
+        height, width, depth = img.shape
+        min_height = height // height_cut
+        min_width = width // width_cut
+        diff_width = min_width if width == width_cut * min_width else width - width_cut * min_width + min_width
+        diff_height = min_height if height == height_cut * min_height else height - height_cut * min_height + min_height
+        for j in range(height_cut):
+            for i in range(width_cut):
+                #  切割的时候如果是靠边的最后一个就将图片加宽，以便保持原本的尺寸
+                left_h = j * min_height
+                left_w = i * min_width
+                right_h = left_h + min_height
+                right_w = left_w + min_width
+                if i == width_cut - 1 or j == height_cut - 1:
+                    if i == width_cut - 1:
+                        right_w = width if width >= width_cut * min_width else (left_w + diff_width)
+                    if j == height_cut - 1:
+                        right_h = height if height >= height_cut * min_height else (left_h + diff_height)
+                sub_img = img[left_h: right_h, left_w: right_w]
+                cv2.imencode('.jpg', sub_img)[1].tofile(
+                    os.path.join(source_min_img_path, os.path.split(img_path)[1][:-4] + '_{}{}'.format(j, i) + '.jpg'))
 
 
 def image_compose2(source_img_path, source_min_img_path, res_pic_path):
@@ -50,7 +73,6 @@ def image_compose2(source_img_path, source_min_img_path, res_pic_path):
     diff_height = min_height if diff_height == 0 else diff_height
 
     print(diff_width, diff_height)
-
     new_image = Image.new('RGB',
                           (min_width * (width_cut - 1) + diff_width, min_height * (height_cut - 1) + diff_height))
 
@@ -63,3 +85,6 @@ def image_compose2(source_img_path, source_min_img_path, res_pic_path):
                                                                                                           i) + '.png'))
             new_image.paste(temp_images, (i * min_width, j * min_height))
     return new_image.save(res_pic_path)
+
+# cut_big_pic(img_path, source_min_img_path, 4, 4)
+# image_compose2(source_img_path, source_min_img_path, res_pic_path)
