f9cc6e354d0e6b76dadf3018784958c1292b8db5
tongchangD <36293393+tongchangD@users.noreply.github.com>
2021-07-29 22:57:08 +0800

    增加 双三次插值算法

--- bicubic_interpolation.py
+++ bicubic_interpolation.py
@@ -77,6 +77,40 @@ def NN_interpolation(img,dstH,dstW):
     return retimg
 
 
+# 产生16个像素点不同的权重
+def BiBubic(x):
+    x=abs(x)
+    if x<=1:
+        return 1-2*(x**2)+(x**3)
+    elif x<2:
+        return 4-8*x+5*(x**2)-(x**3)
+    else:
+        return 0
+
+# 双三次插值算法
+# dstH为目标图像的高，dstW为目标图像的宽
+def BiCubic_interpolation(img,dstH,dstW):
+    scrH,scrW,_=img.shape
+    #img=np.pad(img,((1,3),(1,3),(0,0)),'constant')
+    retimg=np.zeros((dstH,dstW,3),dtype=np.uint8)
+    for i in range(dstH):
+        for j in range(dstW):
+            scrx=i*(scrH/dstH)
+            scry=j*(scrW/dstW)
+            x=math.floor(scrx)
+            y=math.floor(scry)
+            u=scrx-x
+            v=scry-y
+            tmp=0
+            for ii in range(-1,2):
+                for jj in range(-1,2):
+                    if x+ii<0 or y+jj<0 or x+ii>=scrH or y+jj>=scrW:
+                        continue
+                    tmp+=img[x+ii,y+jj]*BiBubic(ii-u)*BiBubic(jj-v)
+            retimg[i,j]=np.clip(tmp,0,255)
+    return retimg
+
+
 
 # img = cv2.imread("../paojie_g.jpg",0).astype(np.float)
 # out = double_linear(img,2).astype(np.uint8)
@@ -96,4 +130,11 @@ def NN_interpolation(img,dstH,dstW):
 # image1=NN_interpolation(image,image.shape[0]*2,image.shape[1]*2)
 # image1=Image.fromarray(image1.astype('uint8')).convert('RGB')
 # image1.save('out.png')
+
+
+# im_path='../paojie.jpg'
+# image=np.array(Image.open(im_path))
+# image2=BiCubic_interpolation(image,image.shape[0]*2,image.shape[1]*2)
+# image2=Image.fromarray(image2.astype('uint8')).convert('RGB')
+# image2.save('BiCubic_interpolation.jpg')
 
