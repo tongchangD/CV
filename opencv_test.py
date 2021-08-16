f0b817094b30839eb795cd1646fb95f60ee09ae4
tongchangD <36293393+tongchangD@users.noreply.github.com>
2021-08-16 14:07:47 +0800

    Update opencv_test.py

--- opencv_test.py
+++ opencv_test.py
@@ -8,3 +8,34 @@ def acquire_RBG(path):
   R = img1[:, :, 2]
   print("B\n",B,"\nG\n",G,"\nR\n",R)
 
+# BGR 图像转bcbcr 
+def bgr_to_ycbcr(one):
+    one = one.astype('float32')
+    (B, G, R) = cv2.split(one)
+    Y = 0.299 * R + 0.587 * G + 0.114 * B
+    Cr = (R - Y) * 0.713 + 0.5
+    Cb = (B - Y) * 0.564 + 0.5
+    return cv2.merge([Y, Cr, Cb])
+# ycbcr图转bgr
+def ycbcr_to_bgr(one):
+    one = one.astype('float32')
+    Y, Cr, Cb = cv2.split(one)
+
+    B = (Cb - 0.5) * 1. / 0.564 + Y
+    R = (Cr - 0.5) * 1. / 0.713 + Y
+    G = 1. / 0.587 * (Y - 0.299 * R - 0.114 * B)
+
+    return cv2.merge([B, G, R])
+
+img=cv2.imread("/home/tcd/Desktop/image-20210816134543164.png")
+
+cv2.imshow("source",img)
+
+Y=bgr_to_ycbcr(img)
+# BGR=ycrcb_to_bgr(img)
+cv2.imshow("Y",Y)
+cv2.imwrite("Y.jpg",Y)
+# cv2.imshow("BGR",BGR)
+cv2.waitKey(0)
+print("done")
+
